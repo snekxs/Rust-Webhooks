680848d0984c958112aae7ca1bd00b1f360df7a8
Snekxs <creepergang321@gmail.com>
2023-05-27 13:43:41 -0600

    Added Client

--- src/lib.rs
+++ src/lib.rs
@@ -2,33 +2,41 @@ use reqwest::{Client, Response};
 use serde_json::{json, Value};
 use std::error::Error as StdError;
 
-pub async fn send_hook(hook_url: &str, content: &str) -> Result<(), Box<dyn StdError>> {
-    let client = Client::new();
-    let payload = json!({"content": content});
-
-    let response = post_json(&client, hook_url, &payload).await?;
-
-    // Handle the response as needed
-    if response.status().is_success() {
-        Ok(())
-    } else {
-        Err(format!("Unexpected response: {:?}", response).into())
-    }
+pub struct DiscordWebhook {
+    client: Client,
+    hook_url: String,
 }
 
-async fn post_json(
-    client: &Client,
-    url: &str,
-    payload: &Value,
-) -> Result<Response, Box<dyn StdError>> {
-    let body = serde_json::to_string(payload)?;
+impl DiscordWebhook {
+    pub fn new(hook_url: &str) -> Self {
+        DiscordWebhook {
+            client: Client::new(),
+            hook_url: hook_url.to_owned(),
+        }
+    }
 
-    let response = client
-        .post(url)
-        .header("Content-Type", "application/json")
-        .body(body)
-        .send()
-        .await?;
+    pub async fn send(&self, content: &str) -> Result<(), Box<dyn StdError>> {
+        let payload = json!({"content": content});
+        let response = self.post_json(&payload).await?;
 
-    Ok(response)
+        if response.status().is_success() {
+            Ok(())
+        } else {
+            Err(format!("Unexpected response: {:?}", response).into())
+        }
+    }
+
+    async fn post_json(&self, payload: &Value) -> Result<Response, Box<dyn StdError>> {
+        let body = serde_json::to_string(payload)?;
+
+        let response = self
+            .client
+            .post(&self.hook_url)
+            .header("Content-Type", "application/json")
+            .body(body)
+            .send()
+            .await?;
+
+        Ok(response)
+    }
 }
